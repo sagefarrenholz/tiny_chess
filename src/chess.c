122fddb99da492019a727f05b54e5c5494247619
Sage <farrenholz@protonmail.com>
2019-08-01 19:25:29 -0400

    started work on main loop. began work on piece move logic.

--- src/chess.c
+++ src/chess.c
@@ -1,19 +1,138 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "chesss.h" 
+
+typedef struct Player {
+
+	uint8_t* board;		//The board this player is playing on.
+	uint8_t team;		//Player's color.
+	
+
+
+} Player;
 
 int main() {
 
-	#pragma warning(disable:4996)
-	FILE* titlescreen = fopen("../art/screen/title.txt", "r+b");
-	char* inbuff = malloc(32);
-	char* outbuff = malloc(SIZE_MAX);
-	//printf("Welcome to \nEnter your name\n>");
-	fread(outbuff, 1, 300, titlescreen);
-	if ("%s", scanf(inbuff)) {
-		printf("An error occured.");
-	}
+	/*
+	In commandline chess a 64 byte array repesents all of the pieces
+	on the board and also empty spaces.
+
+	A NULL byte (0x00) is an empty space
+	For a space with a piece, the type of piece is repesented by the last 3 bits:
+	001 Pawn
+	010 Rook
+	011 Knight
+	100 Bishop
+	101 Queen
+	110 King
+
+	The color of the piece is repesented by the 4th bit from the right.
+	0 Black
+	1 White
+
+	Examples
+	0b00001011 White Knight
+	0x05 Black Queen
+
+	The board is indexed from left to right, bottom to top.
+	A1 is represented by index 0.
+	H1 is represented by index 7.
+	H8 is represented by index 63.
+	*/
+
+		Player* player = (Player*) malloc(sizeof(Player));
+		if (player) {
+			//Intro sequence.
+			//Print title screen here
+
+			//Command / Game loop.
+			char* cmdbuff = (char*)calloc(64, sizeof(char));
+			while (1) {
+				time_t prevtime = time(NULL);	//Get current time.
+				time_t currtime;
+				//If deltatime >= 1 sec redraw turn clock; 1hz.
+				int arg; //Number of command arguments.
+				printf("Type \"help\" for a list of commands\n>");
+				#pragma warning(disable:4996)
+				while (arg = scanf("%64s", cmdbuff)) {
+
+				
+					arg--;
+				} 
+
+			}
 
 
-
+			//End Sequence
+			free(cmdbuff);
+		}
 	return 0;
 }
+
+uint8_t coortoidx(char* coor) {
+	uint8_t y = coor[1];
+	uint8_t x = 0;
+	switch (coor[0]) {
+	case 'a':
+		x = 1;
+		break;
+	case 'b':
+		x = 2;
+		break;
+	case 'c':
+		x = 3;
+		break;
+	case 'd':
+		x = 4;
+		break;
+	case 'e':
+		x = 5;
+		break;
+	case 'f':
+		x = 6;
+		break;
+	case 'g':
+		x = 7;
+		break;
+	case 'h':
+		x = 8;
+		break;
+	}
+	return x * y - 1;
+}
+
+/*
+//White side.
+player->board[0] = 0x0A;	//White Rook A1
+player->board[1] = 0x0B;	//White Knight B1
+player->board[2] = 0x0C;	//White Bishop C1
+player->board[3] = 0x0D;	//White Queen D1
+player->board[4] = 0x0E;	//White King E1
+player->board[5] = 0x0C;	//White Bishop F1
+player->board[6] = 0x0B;	//White Knight G1
+player->board[7] = 0x0A;	//White Rook H1
+player->board[8] = 0x09;	//White Pawn A2
+player->board[9] = 0x09;	//White Pawn B2
+player->board[10] = 0x09;	//White Pawn C2
+player->board[11] = 0x09;	//White Pawn D2
+player->board[12] = 0x09;	//White Pawn E2
+player->board[13] = 0x09;	//White Pawn F2
+player->board[14] = 0x09;	//White Pawn G2
+player->board[15] = 0x09;	//White Pawn H2
+
+//Black Side
+player->board[48] = 0x01;	//Black Pawn A7
+player->board[49] = 0x01;	//Black Pawn B7
+player->board[50] = 0x01;	//Black Pawn C7
+player->board[51] = 0x01;	//Black Pawn D7
+player->board[52] = 0x01;	//Black Pawn E7
+player->board[53] = 0x01;	//Black Pawn F7
+player->board[54] = 0x01;	//Black Pawn G7
+player->board[55] = 0x01;	//Black Pawn H7
+player->board[56] = 0x02;	//Black Rook A8
+player->board[57] = 0x03;	//Black Knight B8
+player->board[58] = 0x04;	//Black Bishop C8
+player->board[59] = 0x05;	//Black Queen D8
+player->board[60] = 0x06;	//Black King E8
+player->board[61] = 0x04;	//Black Bishop F8
+player->board[62] = 0x03;	//Black Knight G8
+player->board[63] = 0x02;	//Black Rook H8
+*/
